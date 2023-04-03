58af64111cf63051aab2f6698aeb43e27ae14b5f
Rosano Coutinho <pub@xeari.com>
2023-04-03 12:07:43 +0700

    Lint

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,43 +3,43 @@ import { withMermaid } from "vitepress-plugin-mermaid";
 
 export default withMermaid(
 
-// https://vitepress.dev/reference/site-config
-defineConfig({
-  title: "Rosanoverse",
-  description: "Mapping my universe",
-  themeConfig: {
+  // https://vitepress.dev/reference/site-config
+  defineConfig({
+    title: "Rosanoverse",
+    description: "Mapping my universe",
+    themeConfig: {
 
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
+      // https://vitepress.dev/reference/default-theme-config
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Examples', link: '/markdown-examples' }
+      ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
+      sidebar: [
+        {
+          text: 'Examples',
+          items: [
+            { text: 'Markdown Examples', link: '/markdown-examples' },
+            { text: 'Runtime API Examples', link: '/api-examples' }
+          ]
+        }
+      ],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/rosano/docs' }
-    ],
+      socialLinks: [
+        { icon: 'github', link: 'https://github.com/rosano/docs' }
+      ],
 
-    editLink: {
-      pattern: 'https://github.com/rosano/docs/edit/master/:path',
-      text: 'Edit this page on GitHub',
+      editLink: {
+        pattern: 'https://github.com/rosano/docs/edit/master/:path',
+        text: 'Edit this page on GitHub',
+      },
+          
     },
-        
-  },
 
-  // optionally, you can pass MermaidConfig
-  mermaid: {
-    // refer https://mermaid.js.org/config/setup/modules/mermaidAPI.html#mermaidapi-configuration-defaults for options
-  },
-}),
-
+    // optionally, you can pass MermaidConfig
+    mermaid: {
+      // refer https://mermaid.js.org/config/setup/modules/mermaidAPI.html#mermaidapi-configuration-defaults for options
+    },
+  }),
+  
 );
